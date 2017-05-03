f2f1bcf7d0f8522cf287165777135a0d964527e8
William A. Rowe Jr <wrowe@rowe-clan.net>
2017-05-03 12:12:23 -0500

    Push Re-license of AL 2.0; Text reviewed by David

--- instance-conf/httpd-2.4/_instance/bin/httpdctl.ps1
+++ instance-conf/httpd-2.4/_instance/bin/httpdctl.ps1
@@ -1,6 +1,20 @@
 ﻿#
-# Copyright © 2017 Pivotal Software, Inc. All rights reserved.
+# Pivotal OSS Instance Creation Schema for Apache HTTP Server
 #
+# Copyright (c) 2017 Pivotal Software, Inc.
+#
+# Pivotal licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # httpdctl.ps1  This script takes care of installing, starting and stopping
 #               Pivotal Web Server services on the Windows platform, following
 #               the same syntax as the unix httpdctl script.
